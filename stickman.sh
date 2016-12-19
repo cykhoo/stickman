141db783ee2f80543c5a9074178970570b51e71e
Chong-Yee Khoo <mail@cykhoo.com>
2016-12-19 13:48:58 +0100

    initial commit

---
+++ stickman.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+for i in {1..10000}
+do
+  cat frames/frame1
+  sleep 0.1
+  clear
+  cat frames/frame2
+  sleep 0.1
+  clear
+  cat frames/frame3
+  sleep 0.1
+  clear
+  cat frames/frame4
+  sleep 0.1
+  clear
+  cat frames/frame5
+  sleep 0.1
+  clear
+  cat frames/frame6
+  sleep 0.1
+  clear
+  cat frames/frame7
+  sleep 0.1
+  clear
+  cat frames/frame8
+  sleep 0.1
+  clear
+  cat frames/frame9
+  sleep 0.1
+  clear
+  cat frames/frame10
+  sleep 0.1
+  clear
+done
